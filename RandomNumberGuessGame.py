913c2f1a79a44d740f6fa8ba0df0e1b7d81339ae
Kanan Guluzada <98650920+guluzada@users.noreply.github.com>
2023-02-25 01:11:32 +0300

    Update RandomNumberGuessGame.py

--- RandomNumberGuessGame.py
+++ RandomNumberGuessGame.py
@@ -6,7 +6,6 @@ print("Welcome! Do you feel lucky today? Let's check it then :D")
 print("Enter a range that you want to pick a number from!")
 range_min = int(input("min: "))
 range_max = int(input("max: "))
-range_difference = range_max - range_min
 random_number = random.randint(range_min, range_max)
 while user_input != random_number:
     user_input = int(input("Predict the number: "))
@@ -18,7 +17,6 @@ while user_input != random_number:
         print("Nope, go up :)")
         attemps_count += 1
     else:
-        
         time.sleep(1)
         print(".")
         time.sleep(1)
@@ -27,5 +25,4 @@ while user_input != random_number:
         print(".")
         print("PERFECT!!! YOU ARE SO SMART!!! ;) ")
         print("You tried " + str(attemps_count) + " times and found.")
-
         continue
