befefe1c210238b99a1b0a6f2ef70918f8361080
Kanan Guluzada <98650920+guluzada@users.noreply.github.com>
2023-02-26 10:55:45 +0300

    Update RandomNumberGuessGame.py
    added point system

--- RandomNumberGuessGame.py
+++ RandomNumberGuessGame.py
@@ -7,15 +7,20 @@ print("Enter a range that you want to pick a number from!")
 range_min = int(input("min: "))
 range_max = int(input("max: "))
 random_number = random.randint(range_min, range_max)
+range_difference = range_max - range_min
+points = 100
 while user_input != random_number:
     user_input = int(input("Predict the number: "))
 
     if user_input > random_number:
         print("Nope, go down :)")
         attemps_count += 1
+        points = points - (100 / (range_difference))
+        
     elif user_input < random_number:
         print("Nope, go up :)")
         attemps_count += 1
+        points = points - (100 / (range_difference))
     else:
         time.sleep(1)
         print(".")
@@ -24,5 +29,10 @@ while user_input != random_number:
         time.sleep(1)
         print(".")
         print("PERFECT!!! YOU ARE SO SMART!!! ;) ")
-        print("You tried " + str(attemps_count) + " times and found.")
+        
+        print("You tried " + str(attemps_count) + " times and found. And your points:" + str(points))
         continue
+
+
+
+#rangediff - attemp
